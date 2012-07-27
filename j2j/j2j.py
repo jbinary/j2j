fd0adac2ee2111b672912ce64ae1205d00cc9ae5
Malinovskiy Dmitry <dmitry@hidevlab.com>
2012-07-27 19:43:28 +0700

    Deleted comments

--- j2j/j2j.py
+++ j2j/j2j.py
@@ -21,7 +21,6 @@ class j2jComponent(TwilixComponent):
         self.config = config
         self.VERSION = version
         self.startTime = None
-        #self.subscribed = SubscribedList(config)
         self.online = []           
 
     def init(self):
@@ -41,38 +40,4 @@ Jabber to jabber gateway')
         self.vcard.init(self.disco)
         self.disco.init()
         self.getOnline()
-        #self.lc = task.LoopingCall(self.updateStatus)
-        #self.lc.start(900)
         print 'Connected!'
-"""
-    def addSubscr(self, from_, to):
-        self.subscribed.add_subscr(from_, to)
-    
-    def rmSubscr(self, from_, to):
-        self.subscribed.rm_subscr(from_, to)
-    
-    def addOnlinesubscr(self, from_, to):
-        if (from_, to) not in self.online:
-            self.online.append((from_, to))
-            
-    def rmOnlinesubscr(self, from_, to):
-        if (from_, to) in self.online:
-            self.online.remove((from_, to))
-    
-    def getOnline(self):
-        for from_, to in self.subscribed.subscr_list:
-            reply = Presence(
-                          to=from_,
-                          from_=to,
-                          type_='probe',                          
-                        )
-            self.dispatcher.send(reply)
-    
-    def _result(self, respond, from_, to):
-        reply = Presence(
-                          to=from_,
-                          from_=to,
-                          status=respond,                          
-                        )
-        self.dispatcher.send(reply)
-        """
