fbe2e1457d6f9708044429411f6fbf53e7658130
Malinovskiy Dmitry <dmitry@hidevlab.com>
2012-08-02 18:34:08 +0700

    Added close method

--- j2j/dbase.py
+++ j2j/dbase.py
@@ -7,12 +7,17 @@ class UserBase(object):
 
     def addUser(self, key, name, pswrd):
         self.base[key] = name, pswrd
+        self.base.sync()
 
     def removeUser(self, key):
         del self.base[key]
+        self.base.sync()
 
     def keyInBase(self, key):
         return key in self.base.keys()
 
     def getUser(self, key):
         return self.base[key]
+
+    def close(self):
+        self.base.close()
