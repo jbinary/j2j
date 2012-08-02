113642cd0657bcdfe493ac5e1ffa05937dbb2409
Malinovskiy Dmitry <dmitry@hidevlab.com>
2012-08-02 18:36:10 +0700

    Splitted old class in two handlers

--- j2j/subscribe.py
+++ j2j/subscribe.py
@@ -1,5 +1,13 @@
+from twisted.internet.defer import inlineCallbacks, returnValue
+from twisted.words.protocols.jabber.sasl import SASLAuthError
+from twisted.internet.error import DNSLookupError
+
 from twilix.stanzas import Presence
 from twilix.base.myelement import EmptyStanza
+from twilix import errors
+
+from j2jClient import j2jClient, ClientPool
+from j2jClient import WrongClientException, DuplicateClientsException
 
 class SubscrHandler(Presence):
 
@@ -14,17 +22,51 @@ class SubscrHandler(Presence):
         reply1.type_ = 'subscribed'
         return reply1, self.probeHandler()
 
-    def availableHandler(self):
-        return self.get_reply()
-
-    def unavailableHandler(self):
-        reply = self.get_reply()
-        reply.type_ = 'unavailable'
-        return reply
-
     def subscribedHandler(self):
         return EmptyStanza()
 
     def unsubscribedHandler(self):
         reply = Presence(to=self.from_, from_=self.to, type_='unsubscribed')
+        return reply
+
+class PresenceHandler(Presence):
+
+    def __init__(self, *args, **kwargs):
+        super(PresenceHandler, self).__init__(*args, **kwargs)
+        self.pool = ClientPool()
+
+    def clean_from_(self, value):
+        key = str(value.userhost())
+        try:
+            username, password = self.host.dbase.getUser(key)
+        except KeyError:
+            raise errors.RegistrationRequiredException
+        self.username, self.password = username, password
+        return value
+
+    @inlineCallbacks
+    def availableHandler(self):
+        if self.username in self.pool.pool.keys():
+            returnValue(self.get_reply())
+        client = j2jClient(self.username)
+        try:
+            yield client.connect(self.password)
+            self.pool.addClient(self.username, client)
+        except DNSLookupError:
+            raise errors.NotAcceptableException
+        except SASLAuthError:
+            raise errors.NotAuthorizedException
+        except DuplicateClientsException:
+            pass
+        except Exception:
+            raise errors.InternalServerErrorException
+        returnValue(self.get_reply())
+
+    def unavailableHandler(self):
+        try:
+            self.pool.removeClient(self.username)
+        except WrongClientException:
+            pass
+        reply = self.get_reply()
+        reply.type_ = 'unavailable'
         return reply
