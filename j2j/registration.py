8b08cc88e22dd96b21e6591dea5407bfb1684705
Malinovskiy Dmitry <dmitry@hidevlab.com>
2012-08-02 21:06:29 +0700

    Added pool of clients

--- j2j/registration.py
+++ j2j/registration.py
@@ -1,21 +1,19 @@
+from twisted.internet import reactor
 from twilix.register import RegisterQuery
 from twilix import fields
 from twilix import errors
 from twilix.stanzas import Presence
 
-from dbase import UserBase
-
 class RegisterHandler(RegisterQuery):
 
     username = fields.StringNode('username', required=False)
     password = fields.StringNode('password', required=False)
-    base = UserBase('users')
 
     def getHandler(self):
         from_ = self.iq.from_
         reply = RegisterHandler(parent=self.makeResult())
-        if self.base.keyInBase(str(from_.userhost())):
-            reply.username, reply.password = self.base.getUser(
+        if self.host.dbase.keyInBase(str(from_.userhost())):
+            reply.username, reply.password = self.host.dbase.getUser(
                                                         str(from_.userhost()))
             reply.registered = ''
             return reply
@@ -26,11 +24,11 @@ class RegisterHandler(RegisterQuery):
 
     def setHandler(self):
         from_ = self.iq.from_
-        if not self.base.keyInBase(str(from_.userhost())):
+        if not self.host.dbase.keyInBase(str(from_.userhost())):
             if self.aremove:
                 raise errors.RegistrationRequiredException
-            self.base.addUser(str(from_.userhost()),
-                              self.username, self.password)
+            self.host.dbase.addUser(str(from_.userhost()),
+                                    self.username, self.password)
             reply1 = RegisterHandler(parent=self.makeResult())
             reply1.username, reply1.password = self.username, self.password
             reply2 = Presence(from_=self.iq.to,
@@ -38,9 +36,14 @@ class RegisterHandler(RegisterQuery):
             return reply1, reply2
         else:
             if self.aremove:
-                self.base.removeUser(str(from_.userhost()))
+                self.host.dbase.removeUser(str(from_.userhost()))
                 return self.makeResult()
-            self.base.addUser(str(from_.userhost()),
+            user, passw = self.host.dbase.getUser(str(from_.userhost()))
+            if self.username != user:
+                for client in self.host.pool.pool.values():
+                    client.xmlstream.sendFooter()
+                    reactor.callLater(1, client.connector.disconnect)
+            self.host.dbase.addUser(str(from_.userhost()),
                               self.username, self.password)
             return self.makeResult()
         return self.makeResult()
