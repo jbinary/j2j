8b8a5c436d0719c43d12782e6d4e428351819ecc
Malinovskiy Dmitry <dmitry@hidevlab.com>
2012-08-21 18:17:27 +0700

    Now users database works with sqlite

--- j2j/dbase.py
+++ j2j/dbase.py
@@ -1,23 +1,55 @@
-import shelve
+import sqlite3
 
 class UserBase(object):
 
     def __init__(self, path):
-        self.base = shelve.open(path)
+        self.base = sqlite3.connect(path)
+        c = self.base.cursor()
+        c.execute('CREATE TABLE IF NOT EXISTS users \
+                        (id integer PRIMARY KEY, \
+                         user_jid text UNIQUE NOT NULL,\
+                         guest_jid text NOT NULL,\
+                         guest_pswd text NOT NULL)')
+        self.base.commit()
+        c.close()
 
-    def addUser(self, key, name, pswrd):
-        self.base[key] = name, pswrd
-        self.base.sync()
+    def addUser(self, userJID, guestJID, guestPswd):
+        c = self.base.cursor()
+        t = (userJID, guestJID, guestPswd)
+        c.execute('INSERT INTO users (user_jid, guest_jid, guest_pswd) \
+                        VALUES (?,?,?)', t)
+        self.base.commit()
+        c.close()
 
-    def removeUser(self, key):
-        del self.base[key]
-        self.base.sync()
+    def updateUser(self, userJID, guestJID, guestPswd):
+        c = self.base.cursor()
+        t = (guestJID, guestPswd, userJID)
+        c.execute('UPDATE users SET guest_jid = ?, guest_pswd = ? \
+                        WHERE user_jid = ?', t)
+        self.base.commit()
+        c.close()
 
-    def keyInBase(self, key):
-        return key in self.base.keys()
+    def userInBase(self, userJID):
+        c = self.base.cursor()
+        c.execute('SELECT * FROM users WHERE user_jid = ?', (userJID, ))
+        ret = c.fetchone()
+        c.close()
+        if ret is None:
+            return False
+        return True
 
-    def getUser(self, key):
-        return self.base[key]
+    def removeUser(self, userJID):
+        c = self.base.cursor()
+        c.execute('DELETE FROM users WHERE user_jid = ?', (userJID, ))
+        self.base.commit()
+        c.close()
 
-    def close(self):
-        self.base.close()
+    def getGuestJID(self, userJID):
+        c = self.base.cursor()
+        c.execute('SELECT guest_jid, guest_pswd FROM users \
+                        WHERE user_jid = ?', (userJID, ))
+        t = c.fetchone()
+        c.close()
+        if t == ():
+            raise KeyError("There's no such user (%s) in the base!" % userJID)
+        return t
